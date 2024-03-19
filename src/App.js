5a3e3bf49d13ad218802c4ac6aabdbc773bd074b
atilebalieva <aitilebalieva@gmail.com>
2024-03-19 13:26:37 -0500

    Added Navigation.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import VerificationSuccess from "./pages/LoginPage.js/VerificationSuccess";
-import NewAccount from "./pages/LoginPage.js/NewAccount";
-import Signin from "./pages/LoginPage.js/SignIn";
-import VerifyEmailPage from "./pages/LoginPage.js/VerifyEmailPage";
-import ResetPasswordPage from "./pages/LoginPage.js/ResetPasswordPage";
-import UpdateUsersAccount from "./pages/LoginPage.js/UpdateUsersAccount";
+/* import VerificationSuccess from "./pages/LoginPage/VerificationSuccess";
+import NewAccount from "./pages/LoginPage/NewAccount";
+import Signin from "./pages/LoginPage/SignIn";
+import VerifyEmailPage from "./pages/LoginPage/VerifyEmailPage";
+import ResetPasswordPage from "./pages/LoginPage/ResetPasswordPage";
+import UpdateUsersAccount from "./pages/LoginPage/UpdateUsersAccount"; */
+import Navigation from "./components/Navbar/Navigation";
 
 function App() {
   return (
     <>
-      <NewAccount />
+      {/*       <NewAccount />
       <VerifyEmailPage />
       <VerificationSuccess />
       <Signin />
       <ResetPasswordPage />
-      <UpdateUsersAccount />
+      <UpdateUsersAccount /> */}
+      <Navigation />
     </>
   );
 }
---
+++ src/components/Navbar/Navigation.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./navigation.css";
+import search from "../../assets/images/search.svg";
+import profileIcon from "../../assets/images/profile-icon.svg";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/Logo.svg";
+import burger from "../../assets/images/burger.svg";
+
+function Navigation() {
+  return (
+    <div className="mulish-font navigation d-flex">
+      <div className="nav-logo d-flex">
+        <button type="button" className="nav-burger-btn">
+          <img src={burger} />
+        </button>
+        <Link>
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
+      </div>
+      <div className="d-flex align-items-center">
+        <form action="" className="position-relative me-5">
+          <input type="search" id="search" className="search" placeholder="Search" />
+          <img src={search} className="search-icon" alt="search" />
+        </form>
+        <div>
+          <img src={profileIcon} className="profile-icon" alt="profile-img" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
